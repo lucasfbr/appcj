581eade5d7989208accec9bdb25dc3280d7b2714
Lucas Rosa <lucas-fbr@hotmail.com>
2016-08-28 21:42:19 -0300

    criando rota e view login

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function (req, res) {
+  res.render('login');
+})
+
 module.exports = router;
