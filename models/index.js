1cf323f1ce8f843eb5785172ea83a3b1f880d3e7
Lucas Rosa <lucas-fbr@hotmail.com>
2016-09-01 11:39:17 -0300

    instalando modulos pgsql e criando conexao com heroku database

--- models/index.js
+++ models/index.js
@@ -3,7 +3,8 @@ var path = require('path');
 //feli stren utilizado para ler arquivos e para ler diretórios
 var fs = require('fs');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize('mysql://root:info03_mysql@localhost:3306/passport');
+//var sequelize = new Sequelize('mysql://root:info03_mysql@localhost:3306/passport');
+var sequelize = new Sequelize('postgres://cypgmhmnymmnui:hL91WxCR0eSSJyHfWudjMve4Lx@ec2-54-243-54-21.compute-1.amazonaws.com:5432/d9bebg631g1u7n');
 var lodash = require('lodash');
 var db = [];
 
