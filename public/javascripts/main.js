10e0f55ade5423ebb215fe79bce5bd4e07065e1d
Lucas Rosa <lucas-fbr@hotmail.com>
2016-09-02 16:09:14 -0300

    Aumentando textarea form contato e removendo campo de busca

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,75 +1,75 @@
-$(function() {
+$(function () {
 
 
-  $('a[href*=#]:not([href=#])').not(".carousel-control").click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+    $('a[href*=#]:not([href=#])').not(".carousel-control").click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 
 
-  $('#myTabs a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-  });
+    $('#myTabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    });
 
     var alerta = $("#alert-hiden").val();
 
-    if(alerta === 'msg'){
+    if (alerta === 'msg') {
 
         $('#myModal').modal('show')
 
     }
 
 
-  $("#frmContato").validate({
-      // Define as regras
-      rules:{
-          nome:{
-              // campoNome será obrigatório (required) e terá tamanho mínimo (minLength)
-              required: true
-          },
-          email:{
-              // campoEmail será obrigatório (required) e precisará ser um e-mail válido (email)
-              required: true, email: true
-          },
-          telefone:{
-              // campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
-              required: true,
-              minlength: 10
-          },
-          mensagem:{
-              // campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
-              required: true
-          }
-      },
-      // Define as mensagens de erro para cada regra
-      messages:{
-          nome:{
-              required: "Digite o seu nome",
-          },
-          email:{
-              required: "Digite o seu e-mail para contato",
-              email: "Digite um e-mail válido"
-          },
-          telefone:{
-              required: "Digite o seu telefone",
-              minLength: "Seu telefone deve conter apenas números"
-          },
-          mensagem:{
-              required: "Digite a sua mensagem",
-          }
-      }
-  });
+    $("#frmContato").validate({
+        // Define as regras
+        rules: {
+            nome: {
+                // campoNome será obrigatório (required) e terá tamanho mínimo (minLength)
+                required: true
+            },
+            email: {
+                // campoEmail será obrigatório (required) e precisará ser um e-mail válido (email)
+                required: true, email: true
+            },
+            telefone: {
+                // campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
+                required: true,
+                minlength: 10
+            },
+            mensagem: {
+                // campoMensagem será obrigatório (required) e terá tamanho mínimo (minLength)
+                required: true
+            }
+        },
+        // Define as mensagens de erro para cada regra
+        messages: {
+            nome: {
+                required: "Digite o seu nome",
+            },
+            email: {
+                required: "Digite o seu e-mail para contato",
+                email: "Digite um e-mail válido"
+            },
+            telefone: {
+                required: "Digite o seu telefone",
+                minLength: "Seu telefone deve conter apenas números"
+            },
+            mensagem: {
+                required: "Digite a sua mensagem",
+            }
+        }
+    });
 
-  $('#telefone').mask('(00) 0000-0000');
+    $('#telefone').mask('(00) 0000-0000');
 
 
 });
